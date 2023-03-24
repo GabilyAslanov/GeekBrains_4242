7fbe6ef22efc37b1a0a15a76f5713d24a21296be
GabilyAslanov <aslanov.gabily@gmail.com>
2023-03-24 18:31:48 +0300

    Add Task with two friends and a dog

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -10,9 +10,9 @@ Console.WriteLine("Enter number : ");
 string strNum = Console.ReadLine();
 int num = Convert.ToInt32(strNum);
 
-double sqrDb = Math.Pow(num, 2);
-int sqr = Convert.ToInt32(sqrDb);
-Console.WriteLine($"sqr = {sqrDb}");
+int sqr = num * num;
+
+Console.WriteLine($"sqr = {sqr}");
 
 
 //Напишите программу, которая на вход принимает два числа и проверяет,
@@ -23,8 +23,8 @@ Console.Write("Введите A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-
-if (Math.Pow(numberB, 2) == numberA)
+sqr = Convert.ToInt32(Math.Pow(numberB, 2));
+if (sqr == numberA)
 {
     Console.WriteLine("A является квадратом B");
 }
@@ -75,10 +75,6 @@ else
     Console.WriteLine("Введен неправильный номер");
 }
 
-if (true)
-{
-
-}
 
 switch (numberDayOfWeek)
 {
@@ -86,9 +82,40 @@ switch (numberDayOfWeek)
     case 2: Console.WriteLine("Вторник"); break;
     case 3: Console.WriteLine("Среда"); break;
     case 4: Console.WriteLine("Четверг"); break;
-    case 5: Console.WriteLine("Среда"); break;
-    case 6: Console.WriteLine("Среда"); break;
-    case 7: Console.WriteLine("Среда"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
     default: Console.WriteLine("Введен неправильный номер"); break;
 
 }
+
+
+//Задача с двумя друзьями и собакой 
+
+int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (FirstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (SecondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine($"Количество раз, за которое собака пробежит, равно - {count}");
